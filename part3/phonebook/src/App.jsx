dc53c27e2f9ea0ac0f932507471ba31200a64221
Francis <fn87@drexel.edu>
2023-10-06 19:40:55 -0400

    add json data fetching

--- part3/phonebook/src/App.jsx
+++ part3/phonebook/src/App.jsx
@@ -3,35 +3,13 @@ import Filter from './components/Filter'
 import Form from './components/Form'
 import Display from './components/Display'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { 
-      name: 'Arto Hellas',
-      number: '1-800-TESTING',
-      id: 1 
-    },
-    {
-      name: 'Jane Doe',
-      number: '(442)-841-4329',
-      id: 2
-    },
-    {
-      name: 'Jake from State Farm',
-      number: '1-800-429-3931',
-      id: 3
-    },
-    {
-      name: 'Janice Johnson',
-      number: '(204)-194-5823',
-      id: 4
-    }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
-
   const [newNumber, setNewNumber] = useState('')
-
   const [filter, setFilter] = useState('')
 
   const handleNameChange = (event) => {
@@ -46,6 +24,15 @@ const App = () => {
 
   const filteredNames = filter ? persons.filter(person => person.name.includes(filter)) : persons
 
+  const hook = () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }
+
+  useEffect(hook, [])
 
   return (
     <div>
